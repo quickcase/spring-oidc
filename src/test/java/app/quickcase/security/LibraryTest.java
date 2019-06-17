18ae4194a0a256fdf2bccd83397290d2f9045778
Valentin Laurin <me@vlaurin.eu>
2019-06-17 17:32:59 +0100

    Setup JUnit 5

--- src/test/java/app/quickcase/security/LibraryTest.java
+++ src/test/java/app/quickcase/security/LibraryTest.java
@@ -3,12 +3,13 @@
  */
 package app.quickcase.security;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-public class LibraryTest {
-    @Test public void testSomeLibraryMethod() {
+class LibraryTest {
+    @Test
+    void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        Assertions.assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
     }
 }
