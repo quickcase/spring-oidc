75c9bcd0d71a12c4fea3c499a89ebd45dbdc8fae
Valentin Laurin <me@vlaurin.eu>
2019-07-02 17:35:57 +0100

    Add authentication api
    - Add UserInfo class to hold actual QuickCase user details, decoupled
    from authentication process
    - Add UserAuthenticationToken to bridge UserInfo with authentication
    process

---
+++ api/src/main/java/app/quickcase/security/UserInfo.java
@@ -0,0 +1,78 @@
+package app.quickcase.security;
+
+import lombok.Builder;
+import lombok.ToString;
+import lombok.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Provides QuickCase user information.
+ *
+ * <p>
+ * Hold non-security related user information for QuickCase users such as name and email.
+ *
+ * <p>
+ * Encapsulated in a {@link UserAuthenticationToken} to link with Spring Security.
+ *
+ * @author Valentin Laurin
+ * @since 0.1
+ */
+@Value
+@Builder
+@ToString(onlyExplicitlyIncluded = true) // GDPR: Keep names and emails outside of logs
+public class UserInfo implements Principal, UserDetails {
+    @ToString.Include
+    private final String id;
+    private final String name;
+    private final String email;
+    @ToString.Include
+    private final Set<GrantedAuthority> authorities;
+
+    @Override
+    public String getPassword() {
+        return "N/A";
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public static class UserInfoBuilder {
+        private Set<GrantedAuthority> authorities = new HashSet<>();
+
+        public UserInfoBuilder authorities(String... authorities) {
+            Arrays.stream(authorities)
+                  .map(SimpleGrantedAuthority::new)
+                  .forEach(this.authorities::add);
+            return this;
+        }
+    }
+}
