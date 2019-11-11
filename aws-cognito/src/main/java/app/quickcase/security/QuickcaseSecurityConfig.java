88769cee51d6d31e5b8a7358f984aed47bd22ec7
Valentin Laurin <me@vlaurin.eu>
2019-11-11 15:13:28 +0000

    Update security config to use CognitoTokenServices
    Register a new primary bean for ResourceServerTokenServices to use
    CognitoTokenServices instead of existing bean of that type.

--- aws-cognito/src/main/java/app/quickcase/security/QuickcaseSecurityConfig.java
+++ aws-cognito/src/main/java/app/quickcase/security/QuickcaseSecurityConfig.java
@@ -1,14 +1,25 @@
 package app.quickcase.security;
 
+import app.quickcase.security.cognito.CognitoTokenServices;
 import app.quickcase.security.cognito.CognitoAuthoritiesExtractor;
 import app.quickcase.security.cognito.CognitoPrincipalExtractor;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateFactory;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 /**
- * Configuration to import in Spring application to auto-configure Spring Security
+ * Configuration to import in Spring application to auto-configure Spring Security to work with AWS
+ * Cognito.
+ * This configuration relies on both properties `security.oauth2.resource.jwk.key-set-uri` and
+ * `security.oauth2.resource.user-info-uri` being defined in Spring application.
  *
  * @author Valentin Laurin
  * @since 0.1
@@ -16,13 +27,37 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class QuickcaseSecurityConfig {
 
-    @Bean
-    public PrincipalExtractor principalExtractor() {
-        return new CognitoPrincipalExtractor();
+    private final TokenStore jwkTokenStore;
+    private final ResourceServerProperties sso;
+    private final OAuth2RestOperations restTemplate;
+
+    public QuickcaseSecurityConfig(TokenStore jwkTokenStore,
+                                   ResourceServerProperties sso,
+                                   UserInfoRestTemplateFactory restTemplateFactory) {
+        this.jwkTokenStore = jwkTokenStore;
+        this.sso = sso;
+        this.restTemplate = restTemplateFactory.getUserInfoRestTemplate();
     }
 
     @Bean
-    public AuthoritiesExtractor authoritiesExtractor() {
-        return new CognitoAuthoritiesExtractor();
+    @Primary
+    public ResourceServerTokenServices resourceServerTokenServices() {
+        return new CognitoTokenServices(defaultTokenServices(), userInfoTokenServices());
+    }
+
+    private DefaultTokenServices defaultTokenServices() {
+        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(jwkTokenStore);
+        return defaultTokenServices;
+    }
+
+    private UserInfoTokenServices userInfoTokenServices() {
+        UserInfoTokenServices services = new UserInfoTokenServices(
+                this.sso.getUserInfoUri(), this.sso.getClientId());
+        services.setRestTemplate(this.restTemplate);
+        services.setTokenType(this.sso.getTokenType());
+        services.setAuthoritiesExtractor(new CognitoAuthoritiesExtractor());
+        services.setPrincipalExtractor(new CognitoPrincipalExtractor());
+        return services;
     }
 }
