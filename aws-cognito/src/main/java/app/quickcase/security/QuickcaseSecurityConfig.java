fc78ad03be280e19dd4b7914bc1e045ce9ce8db2
Valentin Laurin <me@vlaurin.eu>
2019-12-02 22:53:58 +0000

    Make machine authentication conditional
    Disable machine authentication by default and allow it to only be
    enabled on-demand. This is primarily done to facilitate testing and
    local use where Cognito may be stubbed, because machine authentication
    mandates the use of valid access token which cannot be issued by a
    stubbed IDAM server. By making machine authentication an opt-in
    behaviour, local use may keep using stub IDAM server.

--- aws-cognito/src/main/java/app/quickcase/security/QuickcaseSecurityConfig.java
+++ aws-cognito/src/main/java/app/quickcase/security/QuickcaseSecurityConfig.java
@@ -4,13 +4,13 @@ import app.quickcase.security.cognito.CognitoTokenServices;
 import app.quickcase.security.cognito.CognitoAuthoritiesExtractor;
 import app.quickcase.security.cognito.CognitoPrincipalExtractor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateFactory;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -27,35 +27,30 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 @Configuration
 public class QuickcaseSecurityConfig {
 
-    private final TokenStore jwkTokenStore;
-    private final ResourceServerProperties sso;
-    private final OAuth2RestOperations restTemplate;
-
-    public QuickcaseSecurityConfig(TokenStore jwkTokenStore,
-                                   ResourceServerProperties sso,
-                                   UserInfoRestTemplateFactory restTemplateFactory) {
-        this.jwkTokenStore = jwkTokenStore;
-        this.sso = sso;
-        this.restTemplate = restTemplateFactory.getUserInfoRestTemplate();
-    }
-
     @Bean
     @Primary
-    public ResourceServerTokenServices resourceServerTokenServices() {
-        return new CognitoTokenServices(defaultTokenServices(), userInfoTokenServices());
+    @Autowired
+    @ConditionalOnProperty(prefix = "quickcase.security.aws-cognito", name = "enable-machine", havingValue = "true")
+    public ResourceServerTokenServices resourceServerTokenServices(
+            TokenStore jwkTokenStore,
+            ResourceServerProperties sso,
+            UserInfoRestTemplateFactory restTemplateFactory) {
+        return new CognitoTokenServices(defaultTokenServices(jwkTokenStore),
+                                        userInfoTokenServices(sso, restTemplateFactory));
     }
 
-    private DefaultTokenServices defaultTokenServices() {
+    private DefaultTokenServices defaultTokenServices(TokenStore jwkTokenStore) {
         final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
         defaultTokenServices.setTokenStore(jwkTokenStore);
         return defaultTokenServices;
     }
 
-    private UserInfoTokenServices userInfoTokenServices() {
-        UserInfoTokenServices services = new UserInfoTokenServices(
-                this.sso.getUserInfoUri(), this.sso.getClientId());
-        services.setRestTemplate(this.restTemplate);
-        services.setTokenType(this.sso.getTokenType());
+    private UserInfoTokenServices userInfoTokenServices(ResourceServerProperties sso,
+                                                        UserInfoRestTemplateFactory restTemplateFactory) {
+        UserInfoTokenServices services = new UserInfoTokenServices(sso.getUserInfoUri(),
+                                                                   sso.getClientId());
+        services.setRestTemplate(restTemplateFactory.getUserInfoRestTemplate());
+        services.setTokenType(sso.getTokenType());
         services.setAuthoritiesExtractor(new CognitoAuthoritiesExtractor());
         services.setPrincipalExtractor(new CognitoPrincipalExtractor());
         return services;
