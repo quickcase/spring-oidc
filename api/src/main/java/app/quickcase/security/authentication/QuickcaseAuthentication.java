57eac2e98ea2f03a5b0a8d314b881311e7a6fd51
Valentin Laurin <me@vlaurin.eu>
2019-12-04 12:25:04 +0000

    Move away from `spring-security-oauth2` module
    `spring-security-oauth2` module has been deprecated in favour of native
    spring-security implementation. This change drops the dependency on
    `spring-security-oauth2` and replace it by the
    `spring-boot-starter-oauth2-resource-server` native module.
    `QuickcaseAuthentication` class is also refactored to not depend on
    `spring-security-oauth2` and be less coupled to a specific module of
    Spring Security.

--- api/src/main/java/app/quickcase/security/authentication/QuickcaseAuthentication.java
+++ api/src/main/java/app/quickcase/security/authentication/QuickcaseAuthentication.java
@@ -1,128 +1,74 @@
 package app.quickcase.security.authentication;
 
 import app.quickcase.security.UserInfo;
-import org.springframework.lang.Nullable;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.OAuth2Request;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
- * QuickCase-flavoured OAuth 2 authentication. This aims at providing a best-effort in consistency
+ * QuickCase-flavoured authentication. This aims at providing a best-effort in consistency
  * between client-based (client credentials) and user-based (code grant, implicit grant) flows.
  */
-public class QuickcaseAuthentication extends OAuth2Authentication {
-    static final String DEFAULT_CLIENT_NAME = "System";
+public class QuickcaseAuthentication extends AbstractAuthenticationToken {
 
-    private final Collection<GrantedAuthority> authorities;
+    private final String accessToken;
+    private final String id;
+    private final String name;
+    private final UserInfo userInfo;
 
     /**
-     * Construct a QuickCase-flavoured OAuth 2 authentication.
-     * Since some grant types don't require user authentication, the user authentication may be null.
+     * Creates a token with the supplied array of authorities.
      *
-     * @param storedRequest      The authorization request (must not be null).
-     * @param userAuthentication The user authentication (possibly null).
+     * @param authorities the collection of <tt>GrantedAuthority</tt>s for the principal
+     *                    represented by this authentication object.
      */
-    public QuickcaseAuthentication(OAuth2Request storedRequest,
-                                   @Nullable Authentication userAuthentication) {
-        super(storedRequest, userAuthentication);
-        this.authorities = buildAuthorities(storedRequest,userAuthentication);
+    public QuickcaseAuthentication(String accessToken, String id, String name, Collection<? extends GrantedAuthority> authorities) {
+        this(accessToken, id, name, authorities, null);
     }
 
-    /**
-     * Unique identifier of the of the request's author.
-     *
-     * @return subject ID if user authentication is available, client ID otherwise
-     */
-    public String getId() {
-        return getUserInfo().map(UserInfo::getId)
-                            .orElse(getOAuth2Request().getClientId());
+    public QuickcaseAuthentication(String accessToken, String id, String name, Collection<? extends GrantedAuthority> authorities, UserInfo userInfo) {
+        super(authorities);
+        this.setAuthenticated(true);
+        this.accessToken = accessToken;
+        this.id = id;
+        this.name = name;
+        this.userInfo = userInfo;
     }
 
-    /**
-     * Display name of the of the request's author.
-     *
-     * @return subject name if user authentication is available, generic `System` otherwise
-     */
-    public String getName() {
-        return getUserInfo().map(UserInfo::getName)
-                            .orElse(DEFAULT_CLIENT_NAME);
+    public String getAccessToken() {
+        return accessToken;
     }
 
-    /**
-     * Email address of the of the request's author.
-     *
-     * @return subject email if user authentication is available, empty otherwise
-     */
+    @Override
+    public Object getCredentials() {
+        return getAccessToken();
+    }
+
     public Optional<String> getEmail() {
         return getUserInfo().map(UserInfo::getEmail);
     }
 
-    /**
-     * User info of the of the request's author.
-     *
-     * @return subject details if user authentication is available, empty otherwise
-     */
-    public Optional<UserInfo> getUserInfo() {
-        if (!isClientOnly() && UserInfo.class.isAssignableFrom(getPrincipal().getClass()))
-            return Optional.ofNullable((UserInfo) getPrincipal());
-        return Optional.empty();
+    public String getId() {
+        return id;
     }
 
-    /**
-     * All authorities of the request's author.
-     *
-     * @return Concatenation of client authorities, client scopes and user authorities.
-     */
     @Override
-    public Collection<GrantedAuthority> getAuthorities() {
-        return authorities;
+    public String getName() {
+        return name;
     }
 
-    private List<GrantedAuthority> buildAuthorities(OAuth2Request storedRequest,
-                                                    Authentication userAuthentication) {
-        final Collection<? extends GrantedAuthority> clientAuthorities = storedRequest.getAuthorities();
-        final Collection<? extends GrantedAuthority> userAuthorities = Optional.ofNullable(userAuthentication)
-                                                                               .map(Authentication::getAuthorities)
-                                                                               .orElse(Collections.emptyList());
-        final Collection<? extends GrantedAuthority> clientScopes = scopeAuthorities(storedRequest.getScope());
-
-        validateAuthorities(clientAuthorities);
-        validateAuthorities(userAuthorities);
-
-        final int totalSize = clientAuthorities.size() + userAuthorities.size() + clientScopes.size();
-
-        ArrayList<GrantedAuthority> temp = new ArrayList<>(totalSize);
-        temp.addAll(clientAuthorities);
-        temp.addAll(userAuthorities);
-        temp.addAll(clientScopes);
-
-        return Collections.unmodifiableList(temp);
+    @Override
+    public Object getPrincipal() {
+        return getId();
     }
 
-    private Collection<? extends GrantedAuthority> scopeAuthorities(Set<String> scopes) {
-        return scopes.stream()
-                     .filter(Objects::nonNull)
-                     .map(SimpleGrantedAuthority::new)
-                     .collect(Collectors.toSet());
+    public Optional<UserInfo> getUserInfo() {
+        return Optional.ofNullable(userInfo);
     }
 
-    private void validateAuthorities(Collection<? extends GrantedAuthority> authorities) {
-        for (GrantedAuthority a : authorities) {
-            if (a == null) {
-                throw new IllegalArgumentException(
-                        "Authorities collection cannot contain any null elements");
-            }
-        }
+    public Boolean isClientOnly() {
+        return !getUserInfo().isPresent();
     }
 }
