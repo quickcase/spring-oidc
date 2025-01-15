ea044bc8d2443eef10a0baf3b578695cac80c94e
Valentin Laurin <valentin.laurin@quickcase.app>
2025-01-15 10:55:29 +0000

    Define precedence for access levels
    Provide a programmitical way of ordering access levels to avoid duplication of ordering in consumer codes

--- api/src/main/java/app/quickcase/spring/oidc/AccessLevel.java
+++ api/src/main/java/app/quickcase/spring/oidc/AccessLevel.java
@@ -1,5 +1,24 @@
 package app.quickcase.spring.oidc;
 
+import lombok.Getter;
+
+@Getter
 public enum AccessLevel {
-    ORGANISATION, GROUP, INDIVIDUAL;
+    ORGANISATION(10),
+    GROUP(20),
+    INDIVIDUAL(30);
+
+    private final int precedence;
+
+    AccessLevel(int precedence) {
+        this.precedence = precedence;
+    }
+
+    public boolean highestPrecedenceThan(AccessLevel accessLevel) {
+        return precedence > accessLevel.getPrecedence();
+    }
+
+    public boolean lowestPrecedenceThan(AccessLevel accessLevel) {
+        return precedence < accessLevel.getPrecedence();
+    }
 }
